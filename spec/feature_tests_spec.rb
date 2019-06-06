e2da93ce05562f2b8826ae3d2116940240316d47
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-06-06 14:13:12 +0100

    add sulfuras logic, refactoring update_quality

--- spec/feature_tests_spec.rb
+++ spec/feature_tests_spec.rb
@@ -2,16 +2,16 @@ require 'gilded_rose'
 
 describe 'Feature Tests' do
 
-  context "updating quality for '+5 Dexterity Vest'" do
+  context "updating quality for a standard item" do
     it "correctly updates quality after over time" do
-      dexterity_vest = [Item.new(name="+5 Dexterity Vest", sell_in=1, quality=11)]
-      gilded_rose = GildedRose.new(dexterity_vest)
+      standard_item = [Item.new(name="Standard item", sell_in=1, quality=11)]
+      gilded_rose = GildedRose.new(standard_item)
       gilded_rose.update_quality()
-      expect(dexterity_vest[0].to_s).to eq "+5 Dexterity Vest, 0, 10"
+      expect(standard_item[0].to_s).to eq "Standard item, 0, 10"
       gilded_rose.update_quality()
-      expect(dexterity_vest[0].to_s).to eq "+5 Dexterity Vest, -1, 8"
+      expect(standard_item[0].to_s).to eq "Standard item, -1, 8"
       gilded_rose.update_quality()
-      expect(dexterity_vest[0].to_s).to eq "+5 Dexterity Vest, -2, 6"
+      expect(standard_item[0].to_s).to eq "Standard item, -2, 6"
     end
   end
 
@@ -28,28 +28,15 @@ describe 'Feature Tests' do
     end
   end
 
-  context "updating quality for 'Elixir of the Mongoose'" do
-    it "correctly updates quality after over time" do
-      mongoose_elixir = [Item.new(name="Elixir of the Mongoose", sell_in=2, quality=4)]
-      gilded_rose = GildedRose.new(mongoose_elixir)
-      gilded_rose.update_quality()
-      expect(mongoose_elixir[0].to_s).to eq "Elixir of the Mongoose, 1, 3"
-      gilded_rose.update_quality()
-      expect(mongoose_elixir[0].to_s).to eq "Elixir of the Mongoose, 0, 2"
-      gilded_rose.update_quality()
-      expect(mongoose_elixir[0].to_s).to eq "Elixir of the Mongoose, -1, 0"
-    end
-  end
-
   context "updating quality for 'Sulfuras, Hand of Ragnaros'" do
     it "correctly updates quality after over time" do
-      sufuras = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=50)]
+      sufuras = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=80)]
       gilded_rose = GildedRose.new(sufuras)
-      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 50"
+      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 80"
       gilded_rose.update_quality()
-      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 50"
+      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 80"
       gilded_rose.update_quality()
-      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 50"
+      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 80"
     end
   end
 
