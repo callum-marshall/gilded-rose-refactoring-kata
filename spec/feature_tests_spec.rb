4b02aafb274605eee2e7394fa99d6c63380e8203
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-06-06 10:32:13 +0100

    add tests

--- spec/feature_tests_spec.rb
+++ spec/feature_tests_spec.rb
@@ -1,33 +1,70 @@
-# ======================================
-# Gilded Rose Requirements Specification
-# ======================================
-#
-# First an introduction to our system:
-#
-# 	- All items have a SellIn value which denotes the number of days we have to sell the item
-# 	- All items have a Quality value which denotes how valuable the item is
-# 	- At the end of each day our system lowers both values for every item
-#
-# Pretty simple, right? Well this is where it gets interesting:
-#
-# 	- Once the sell by date has passed, Quality degrades twice as fast
-# 	- The Quality of an item is never negative
-# 	- "Aged Brie" actually increases in Quality the older it gets
-# 	- The Quality of an item is never more than 50
-# 	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-# 	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-# 	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-# 	Quality drops to 0 after the concert
-#
-# We have recently signed a supplier of conjured items. This requires an update to our system:
-#
-# 	- "Conjured" items degrade in Quality twice as fast as normal items
-#
-# Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
-# still works correctly. However, do not alter the Item class or Items property as those belong to the
-# goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code
-# ownership (you can make the UpdateQuality method and Items property static if you like, we'll cover
-# for you).
-#
-# Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
-# legendary item and as such its Quality is 80 and it never alters.
+require 'gilded_rose'
+
+describe 'Feature Tests' do
+
+  context "updating quality for '+5 Dexterity Vest'" do
+    it "correctly updates quality after over time" do
+      dexterity_vest = [Item.new(name="+5 Dexterity Vest", sell_in=1, quality=11)]
+      gilded_rose = GildedRose.new(dexterity_vest)
+      gilded_rose.update_quality()
+      expect(dexterity_vest[0].to_s).to eq "+5 Dexterity Vest, 0, 10"
+      gilded_rose.update_quality()
+      expect(dexterity_vest[0].to_s).to eq "+5 Dexterity Vest, -1, 8"
+      gilded_rose.update_quality()
+      expect(dexterity_vest[0].to_s).to eq "+5 Dexterity Vest, -2, 6"
+    end
+  end
+
+  context "updating quality for 'Aged Brie'" do
+    it "correctly updates quality after over time" do
+      aged_brie = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
+      gilded_rose = GildedRose.new(aged_brie)
+      gilded_rose.update_quality()
+      expect(aged_brie[0].to_s).to eq "Aged Brie, 1, 1"
+      gilded_rose.update_quality()
+      expect(aged_brie[0].to_s).to eq "Aged Brie, 0, 2"
+      gilded_rose.update_quality()
+      expect(aged_brie[0].to_s).to eq "Aged Brie, -1, 4"
+    end
+  end
+
+  context "updating quality for 'Elixir of the Mongoose'" do
+    it "correctly updates quality after over time" do
+      mongoose_elixir = [Item.new(name="Elixir of the Mongoose", sell_in=2, quality=4)]
+      gilded_rose = GildedRose.new(mongoose_elixir)
+      gilded_rose.update_quality()
+      expect(mongoose_elixir[0].to_s).to eq "Elixir of the Mongoose, 1, 3"
+      gilded_rose.update_quality()
+      expect(mongoose_elixir[0].to_s).to eq "Elixir of the Mongoose, 0, 2"
+      gilded_rose.update_quality()
+      expect(mongoose_elixir[0].to_s).to eq "Elixir of the Mongoose, -1, 0"
+    end
+  end
+
+  context "updating quality for 'Sulfuras, Hand of Ragnaros'" do
+    it "correctly updates quality after over time" do
+      sufuras = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=50)]
+      gilded_rose = GildedRose.new(sufuras)
+      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 50"
+      gilded_rose.update_quality()
+      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 50"
+      gilded_rose.update_quality()
+      expect(sufuras[0].to_s).to eq "Sulfuras, Hand of Ragnaros, 5, 50"
+    end
+  end
+
+  context "updating quality for 'Backstage passes to a TAFKAL80ETC concert'" do
+    it "correctly updates quality after over time" do
+      backstage_pass = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=11, quality=5)]
+      gilded_rose = GildedRose.new(backstage_pass)
+      gilded_rose.update_quality()
+      expect(backstage_pass[0].to_s).to eq "Backstage passes to a TAFKAL80ETC concert, 10, 6"
+      6.times {gilded_rose.update_quality()}
+      expect(backstage_pass[0].to_s).to eq "Backstage passes to a TAFKAL80ETC concert, 4, 19"
+      4.times {gilded_rose.update_quality()}
+      expect(backstage_pass[0].to_s).to eq "Backstage passes to a TAFKAL80ETC concert, 0, 31"
+      gilded_rose.update_quality()
+      expect(backstage_pass[0].to_s).to eq "Backstage passes to a TAFKAL80ETC concert, -1, 0"
+    end
+  end
+end
